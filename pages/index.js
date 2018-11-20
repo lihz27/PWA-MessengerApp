18fbd4c1c9977c75b0613d6b1baf0efa0438b95d
Brian Louie <brianalouie@gmail.com>
2018-11-20 13:20:51 -0800

    Init

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import 'isomorphic-fetch'
+import React from 'react'
+import { connect } from 'react-redux'
+
+import Todo from '../components/Todo'
+
+class Index extends React.Component {
+	static async getInitialProps({ store }) {
+		// Adding a default/initialState can be done as follows:
+		// store.dispatch({ type: 'ADD_TODO', text: 'It works!' });
+		const res = await fetch(
+			'https://api.github.com/repos/ooade/NextSimpleStarter'
+		)
+		const json = await res.json()
+		return { stars: json.stargazers_count }
+	}
+
+	render() {
+		const { stars } = this.props
+		return (
+			<div>
+				<div>
+					<Todo />
+				</div>
+			</div>
+		)
+	}
+}
+
+export default connect()(Index)
---
+++ utils/store.js
@@ -0,0 +1,23 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import { offline } from '@redux-offline/redux-offline';
+import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
+
+import rootReducer from '../reducers'
+
+const enhancers = compose(
+	typeof window !== 'undefined' && process.env.NODE_ENV !== 'production'
+		? window.devToolsExtension && window.devToolsExtension()
+		: f => f
+)
+
+
+
+export default initialState =>
+createStore(
+	rootReducer,
+	initialState,
+	compose(
+		applyMiddleware(),
+		offline(offlineConfig)
+	)
+);
